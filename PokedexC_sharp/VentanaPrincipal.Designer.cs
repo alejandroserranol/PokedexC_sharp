8db3b0f06311ab48d7d1f38db3ea0848a8030e81
alejandroserranol <alejandroserranol@hotmail.com>
2020-03-27 11:00:46 +0100

    Versión con cambio de pokemon

--- PokedexC_sharp/VentanaPrincipal.Designer.cs
+++ PokedexC_sharp/VentanaPrincipal.Designer.cs
@@ -28,13 +28,98 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.izq = new System.Windows.Forms.Button();
+            this.der = new System.Windows.Forms.Button();
+            this.nombrePokemon = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(192, 186);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // izq
+            // 
+            this.izq.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.izq.Location = new System.Drawing.Point(12, 204);
+            this.izq.Name = "izq";
+            this.izq.Size = new System.Drawing.Size(70, 48);
+            this.izq.TabIndex = 1;
+            this.izq.Text = "<";
+            this.izq.UseVisualStyleBackColor = true;
+            this.izq.Click += new System.EventHandler(this.izq_Click);
+            // 
+            // der
+            // 
+            this.der.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.der.Location = new System.Drawing.Point(134, 204);
+            this.der.Name = "der";
+            this.der.Size = new System.Drawing.Size(70, 48);
+            this.der.TabIndex = 2;
+            this.der.Text = ">";
+            this.der.UseVisualStyleBackColor = true;
+            this.der.Click += new System.EventHandler(this.der_Click);
+            // 
+            // nombrePokemon
+            // 
+            this.nombrePokemon.Location = new System.Drawing.Point(9, 255);
+            this.nombrePokemon.Name = "nombrePokemon";
+            this.nombrePokemon.Size = new System.Drawing.Size(195, 61);
+            this.nombrePokemon.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 319);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(192, 119);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(211, 13);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(577, 425);
+            this.dataGridView1.TabIndex = 5;
+            // 
+            // VentanaPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.nombrePokemon);
+            this.Controls.Add(this.der);
+            this.Controls.Add(this.izq);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "VentanaPrincipal";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button izq;
+        private System.Windows.Forms.Button der;
+        private System.Windows.Forms.Label nombrePokemon;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataGridView1;
     }
 }
 
